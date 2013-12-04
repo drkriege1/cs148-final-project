b77dd2af2feda6f652f1dfc2ebd8b0ded6c0e0b8
Doug Krieger <doug.krieger@uvm.edu>
2013-12-04 09:26:34 -0500

    finished coding and debugging sql queries in gallery.php

--- gallery.php
+++ gallery.php
@@ -35,7 +35,6 @@
     		$sql = "select distinct fk_tag_name, fld_img_src from tbl_art_tag, tbl_art where fk_art_id = pk_art_id group by fk_tag_name;";
 			$stmt = $db->prepare($sql);
 			$stmt->execute();
-			$tags = array();
 			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 				$tags[$row[fk_tag_name]] = $row[fld_img_src];
 			}
@@ -50,28 +49,40 @@
     	}
     	
     	// if query is "all", display unfiltered gallery
-    	// ...
     	else if ($_SERVER['QUERY_STRING'] == "all") {
     		require_once "includes/sidebar.php";
-    		$sql = "select fld_name, fld_img_src from tbl_art";
+    		$sql = "select pk_art_id, fld_name, fld_img_src from tbl_art";
 			$stmt = $db->prepare($sql);
 			$stmt->execute();
-			$tags = array();
 			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-				$art[$row[fld_name]] = $row[fld_img_src];
+				$art[$row[pk_art_id]] = array($row[fld_img_src], $row[fld_name]);
 			}
-    		foreach ($art as $name => $img_src) {
-    			echo "<a href='gallery.php?$name'>\n";
-    			echo "<img class='thumbnail' src='$img_src' alt='$name' />\n";
-    			echo "<br><h1>$name</h1></a>\n";
+    		foreach ($art as $id => $imgSrc_name) {
+    			echo "<a href='slide.php?$id'>\n";
+    			echo "<img class='thumbnail' src='$imgSrc_name[0]' alt='$imgSrc_name[1]' />\n";
+    			echo "<br><h1>$imgSrc_name[1]</h1></a>\n";
     		}
-			
     	}
     	
     	// if query is some tag, display gallery filtered by that tag
     	// ...
     	else {
     		require_once "includes/sidebar.php";
+    		$sql = "select fk_art_id, fld_name, fld_img_src from tbl_art_tag, tbl_art where fk_tag_name=? and fk_art_id=pk_art_id group by fk_art_id;";
+			$stmt = $db->prepare($sql);
+			$stmt->execute(array($_SERVER['QUERY_STRING']));
+			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+				$art[$row[fk_art_id]] = array($row[fld_img_src], $row[fld_name]);
+			}
+			if (!$art) {
+				echo "Nothing has this tag...";
+			} else {
+    			foreach ($art as $id => $imgSrc_name) {
+    				echo "<a href='slide.php?$id'>\n";
+    				echo "<img class='thumbnail' src='$imgSrc_name[0]' alt='$imgSrc_name[1]' />\n";
+    				echo "<br><h1>$imgSrc_name[1]</h1></a>\n";
+    			}
+    		}
     	}
     	
     	require_once "includes/footer.php";
