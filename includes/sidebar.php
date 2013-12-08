95f01641a6d9d743d3ae417a0eb000d79fdff4f5
Doug Krieger <doug.krieger@uvm.edu>
2013-12-08 02:24:54 -0500

    modified sidebar.php to exclude tags with no displayed pieces of art

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,11 +1,11 @@
 <?	
 	echo "<a href='gallery.php?all'>[ALL]</a><br>";
 	
-    $sql = "select pk_tag_name from tbl_tag;";
+    $sql = "select distinct fk_tag_name from tbl_art_tag, tbl_art where fld_display = 1 and pk_art_id = fk_art_id group by fk_tag_name";
 	$stmt = $db->prepare($sql);
 	$stmt->execute();
 	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-		$tags[] = $row[pk_tag_name];
+		$tags[] = $row[fk_tag_name];
 	}
 
     foreach ($tags as $tag) {
