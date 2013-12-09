67626bcf3dfdad5d3c65b1f5d76bc39c7311193c
Doug Krieger <doug.krieger@uvm.edu>
2013-12-09 05:37:19 -0500

    began admin section

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,16 @@
+<?	
+	echo "<a href='gallery.php?all'>[ALL]</a><br>";
+	
+    $sql = "select distinct fk_tag_name from tbl_art_tag, tbl_art where fld_display = 1 and pk_art_id = fk_art_id group by fk_tag_name";
+	$stmt = $db->prepare($sql);
+	$stmt->execute();
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		$tags_sidebar[] = $row[fk_tag_name];
+	}
+
+    foreach ($tags_sidebar as $tag) {
+    	$tag = ucfirst($tag);
+    	echo "<a href='gallery.php?$tag'>$tag</a><br>\n";
+    }
+    		
+?>
